51d824f98f56f299c009701abe8f30791167cec9
David Villa Alises <david.villa@gmail.com>
2026-02-17 19:43:25 +0100

    rm tmux conf

--- flow-control/run.sh
+++ flow-control/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-tmux -f .tmux.conf new-session -d -s session
-tmux -f .tmux.conf split-window -h
-tmux -f .tmux.conf send-keys -t session:0.0 "sleep 1; ./client.py 127.0.0.1 2000" C-m
-tmux -f .tmux.conf send-keys -t session:0.1 "./server.py 2000 200" C-m
-tmux -f .tmux.conf split-window -v
-tmux -f .tmux.conf send-keys -t session:0.2 "sleep 70; tmux kill-session -t session" C-m
-tmux -f .tmux.conf attach-session -t session
+tmux new-session -d -s session
+
+tmux set-option -g mouse on
+tmux setw -g monitor-activity on
+tmux set-option -g visual-activity on
+tmux bind-key x kill-session
+tmux set-option -g set-titles off
+
+tmux split-window -h
+tmux send-keys -t session:0.0 "sleep 1; ./client.py 127.0.0.1 2000" C-m
+tmux send-keys -t session:0.1 "./server.py 2000 200" C-m
+tmux split-window -v
+tmux send-keys -t session:0.2 "sleep 70; tmux kill-session -t session" C-m
+tmux attach-session -t session
