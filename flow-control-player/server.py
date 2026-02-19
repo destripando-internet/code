40770594a93bdf5135b8f097219b2a3541c6cacc
David Villa Alises <david.villa@gmail.com>
2026-02-19 15:29:37 +0100

    player/server and player/client are superseded by flow-control ones

--- flow-control-player/server.py
+++ flow-control-player/server.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env -S python3 -u
+# Copyright: See AUTHORS and COPYING
 
 import sys
 import socket
@@ -8,12 +9,30 @@ import atexit
 from time import sleep, monotonic
 
 
-def log(msg):
-    sys.stderr.write(msg)
-    sys.stderr.flush()
+def mprint(msg, end='\n'):
+    print(msg, file=sys.stderr, flush=True, end=end)
 
 
-class EMA:
+class CA_RateMeter:
+    def __init__(self):
+        self.elapsed = 0
+        self.bytes_from_start = 0
+        self.rate = 0
+        self.start_time = monotonic()
+
+    def update(self, chunk_len):
+        self.bytes_from_start += chunk_len
+        self.elapsed = monotonic() - self.start_time
+        if self.elapsed <= 0:
+            return
+
+        self.rate = self.bytes_from_start / self.elapsed
+
+    def __str__(self):
+        return f'CA:{self.rate / 1000:.1f} kB/s'
+
+
+class EMA_RateMeter:
     def __init__(self):
         self.rate = 0
         self.tau = 1.0
@@ -22,36 +41,40 @@ class EMA:
     def _alpha(self, dt):
         return 1.0 - math.exp(-dt / self.tau) if dt > 0 else 1.0
 
-    def update(self, sent_bytes):
+    def update(self, data_len):
         dt = monotonic() - self.last
         alpha = self._alpha(dt)
-        self.rate = alpha * sent_bytes / dt + (1 - alpha) * self.rate
+        self.rate = alpha * data_len / dt + (1 - alpha) * self.rate
         self.last = monotonic()
 
-    def rate_kBps(self):
-        return self.rate / 1000
+    def __str__(self):
+        return f'EMA:{self.rate / 1000:.1f} kB/s'
 
 
-class SMA:
+class SMA_RateMeter:
     def __init__(self, window_size=5):
         self.window_size = window_size
         self.window = collections.deque()
 
-    def update(self, sent_bytes):
+    def update(self, data_len):
         now = monotonic()
-        self.window.append((now, sent_bytes))
+        self.window.append((now, data_len))
 
         # Remove old data outside the window
         while self.window and (now - self.window[0][0]) > self.window_size:
             self.window.popleft()
 
-    def rate_kBps(self):
+    @property
+    def rate(self):
         if not self.window:
             return 0
 
         window_bytes = sum(b for t, b in self.window)
         elapsed = self.window[-1][0] - self.window[0][0]
-        return window_bytes / elapsed / 1000 if elapsed > 0 else 0
+        return window_bytes / elapsed if elapsed > 0 else 0
+
+    def __str__(self):
+        return f'SMA:{self.rate // 1000:.1f} kB/s'
 
 
 class RateTrace:
@@ -60,11 +83,11 @@ class RateTrace:
         self.file = open('stats.csv', 'w')
         atexit.register(self.file.close)
 
-    def update(self, sma, ema):
+    def update(self, ca, ema, sma):
         now = monotonic()
         elapsed = now - self.start
         self.last = now
-        self.file.write(f'{elapsed:.3f},{sma:.3f},{ema:.3f}\n')
+        self.file.write(f'{elapsed:.3f},{ca:.3f},{ema:.3f},{sma:.3f}\n')
         self.file.flush()
 
 
@@ -78,39 +101,39 @@ class Receiver:
         self.trace = RateTrace()
 
     def run(self):
-        self.conn, client = self.sock.accept()
-        self.start_time = monotonic()
-
         try:
+            self.conn, client = self.sock.accept()
+            mprint(f'Client connected: {client}')
             self.receiving()
-        except KeyboardInterrupt:
-            pass
+        except (KeyboardInterrupt, socket.error) as e:
+            mprint(f"\nShutting down server. {e}")
         finally:
             self.sock.close()
 
     def receiving(self):
         self.received = 0
-        self.ema = EMA()
-        self.sma = SMA()
+        self.ca = CA_RateMeter()
+        self.ema = EMA_RateMeter()
+        self.sma = SMA_RateMeter()
         while 1:
             data = self.conn.recv(1024)
             if not data:
                 break
 
-            self.received += len(data)
-            self.ema.update(len(data))
-            self.sma.update(len(data))
-            self.trace.update(self.sma.rate_kBps(), self.ema.rate_kBps())
+            self.received += (chunk_len := len(data))
+            self.ca.update(chunk_len)
+            self.ema.update(chunk_len)
+            self.sma.update(chunk_len)
+            self.trace.update(self.ca.rate, self.ema.rate, self.sma.rate)
             self.show_stats()
 
             sys.stdout.buffer.write(data)
             sys.stdout.buffer.flush()
 
     def show_stats(self):
-        msg = f'received: {self.received//1000:,} kB, '
-        msg += f'EMA: {self.sma.rate_kBps():,.1f} kB/s, '
-        msg += f'SMA: {self.ema.rate_kBps():,.1f} kB/s'
-        log(f'\r {msg} {30 * " "}\r')
+        msg = f'received:{self.received / 1000:,.1f} kB, '
+        msg += f'{self.ca}, {self.ema}, {self.sma}'
+        mprint(f'\r {msg} {30 * " "}', end='\r')
         sleep(0.001)
 
 
