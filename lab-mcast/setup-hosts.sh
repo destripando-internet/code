2a60bea99f4eaf0e21c33601eeda52337a5f681a
David Villa Alises <david.villa@gmail.com>
2026-04-28 15:14:23 +0200

    refactor

--- lab-mcast/setup-hosts.sh
+++ lab-mcast/setup-hosts.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# add route for 10.0/16 for "host"
+ip route replace 10.0.0.0/16 via 10.0.0.3
+ip route replace 224.0.0.0/4 via 10.0.0.3
+
 # add "node2" default route
 docker exec node2 ip route add default via 10.0.5.2
 docker exec node2 ip route add 224.0.0.0/4 via 10.0.5.2
@@ -7,7 +11,3 @@ docker exec node2 ip route add 224.0.0.0/4 via 10.0.5.2
 # add "node3" default route
 docker exec node3 ip route add default via 10.0.3.2
 docker exec node3 ip route add 224.0.0.0/4 via 10.0.3.2
-
-# add route for 10.0/16 for "host"
-ip route replace 10.0.0.0/16 via 10.0.0.3
-ip route replace 224.0.0.0/4 via 10.0.0.3
